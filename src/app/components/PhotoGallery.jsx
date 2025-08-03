e4d7d4762aff24787e2beb5482a2e0fe7d3f848c
Kaarthik Dass Arora <kartikdassarora@gmail.com>
2025-08-03 20:36:37 +0530

    Pushed all files again

--- src/app/components/PhotoGallery.jsx
+++ src/app/components/PhotoGallery.jsx
@@ -15,6 +15,7 @@ export default function PhotoGallery({ onNext }) {
         { id: 2, src: "/images/2.jpg" },
         { id: 3, src: "/images/3.jpg" },
         { id: 4, src: "/images/4.jpg" },
+        { id: 5, src: "/images/5.jpg" },
     ]
 
     return (
